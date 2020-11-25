fe303c9ad2b36433064fbc7f7b763b27f42ef772
Jens hohl <jens.hohl@googlemail.com>
2020-11-25 13:19:10 +0100

    Kotlin 1.4.20 Dependency version bump.

--- app/src/main/kotlin/de/hohl/cleanarchitecturebase/cache/ExampleCacheObject.kt
+++ app/src/main/kotlin/de/hohl/cleanarchitecturebase/cache/ExampleCacheObject.kt
@@ -1,3 +1,6 @@
 package de.hohl.cleanarchitecturebase.cache
 
+/**
+ * Represents an Entity which is stored on the device
+ */
 data class ExampleCacheObject(val id: Int, val name: String, val price: String)
--- app/src/main/kotlin/de/hohl/cleanarchitecturebase/remote/ExampleRemoteEntity.kt
+++ app/src/main/kotlin/de/hohl/cleanarchitecturebase/remote/ExampleRemoteEntity.kt
@@ -1,3 +1,6 @@
 package de.hohl.cleanarchitecturebase.remote
 
+/**
+ * Represents Entity which gets loaded from the backend
+ */
 data class ExampleRemoteEntity(val id: Int, val name: String, val price: String)
